a91d3b64d64eb42ac580c3d49850958d8ef19547
kkiha <113440402+kkiha@users.noreply.github.com>
2022-10-09 14:31:46 +0900

    Javascript complete

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "0427e40535d0d4e18999846c2daee9e8"
+
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = `${data.weather[0].main}, ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
